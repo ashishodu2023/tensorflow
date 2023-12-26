5c10e9f8045a4aa66b817827773b26f9c456b533
averma111 <ashishn8751@outlook.com>
2023-12-26 15:38:41 -0800

    Updated Bulk Predictions

--- CNN/ Cifar10Augmentation/BulkPrediction.py
+++ CNN/ Cifar10Augmentation/BulkPrediction.py
@@ -1,8 +1,11 @@
-import numpy as np
-from matplotlib.pyplot import imread
-from skimage.transform import resize
+import cv2
+import tensorflow as tf
 from tensorflow.keras.models import model_from_json
 from tensorflow.keras.optimizers import SGD
+from keras.datasets import cifar10
+import numpy as np
+import matplotlib.pyplot as plt
+import os
 
 
 # Load Model
@@ -10,12 +13,21 @@ from tensorflow.keras.optimizers import SGD
 class BulkPrediction:
 
     def __init__(self, model_arch, model_weights):
+        self.classes = None
+        self.predictions = None
         self.image = None
         self.images = None
         self.model_arch = model_arch
         self.model_weights = model_weights
-        self.img_name = ['cat-standing.jpg', 'dog.jpg']
         self.model = None
+        self.labels = ['Airplane', 'Automobile', 'Bird', 'Cat', 'Deer', 'Dog', 'Frog', 'Horse', 'Ship', 'Truck']
+        self.R = 5
+        self.C = 5
+
+    # Load Data
+    def load_data(self):
+        (x_train, y_train), (x_test, y_test) = cifar10.load_data()
+        return (x_train, y_train), (x_test, y_test)
 
     # Load Model
     def load_model(self):
@@ -24,36 +36,34 @@ class BulkPrediction:
         self.model.load_weights(self.model_weights)
         print(self.model.summary())
 
-    # Load Images
-    def load_images(self):
-        print("===============Load Test Images===================")
-        self.images = [np.transpose(resize(imread(image), (32, 32)),
-                                    (2, 0, 1)).astype('float32')
-                       for image in self.img_name
-                       ]
-        # self.images = [np.transpose(Image.fromarray(obj=img, mode='F').resize(size=(32, 32), resample=Image.BICUBIC))
-        #                for img in self.img_name]
-        self.images = np.array(self.images) / 255.0
-        print(self.images)
-
     # Train
     def train(self):
         print("===============Compiling Model===================")
         self.model.compile(loss='categorical_crossentropy', optimizer=SGD(), metrics=['accuracy'])
+        print("===============Model Compiled====================")
 
-    def predict(self):
+    def predict(self, x_test, y_test):
         print("===============Predictions===================")
-        self.predictions = self.model.predict(self.images)
-        self.classes = np.argmax(self.predictions, axis=1)
-        print(self.classes)
+        predictions = self.model.predict(x_test)
+        Y_pred_classes = np.argmax(predictions, axis=1)
+        Y_true = np.argmax(y_test, axis=1)
+        fig, axes = plt.subplots(self.R, self.C, figsize=(12, 12))
+        axes = axes.ravel()
+
+        for i in np.arange(0, self.R * self.C):
+            axes[i].imshow(x_test[i])
+            axes[i].set_title("True: %s \nPredict: %s" % (self.labels[Y_true[i]], self.labels[Y_pred_classes[i]]))
+            axes[i].axis('off')
+            plt.subplots_adjust(wspace=1)
+        plt.show()
 
 
 def main():
     bp = BulkPrediction('model.json', 'model.h5')
-    bp.load_model()
+    (x_train, y_train), (x_test, y_test) = bp.load_data()
     bp.load_model()
     bp.train()
-    bp.predict()
+    bp.predict(x_test, y_test)
 
 
 if __name__ == '__main__':
